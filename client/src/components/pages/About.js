0a1d8314de1b823cb773fa5c7593b3aa068f78b0
ShawnYoung72443 <sryoung72443@gmail.com>
2020-09-16 23:20:39 -0400

    add git hub link to about section

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -6,6 +6,8 @@ const About = () => {
       <h1>About This App</h1>
       <p className='my-1'>
         This is a full stack React app for keeping contacts.
+        Made with a Mongo Database and Node RestAPI server utilizing express.
+        This project was created and distrubuted by <a className='git' href='https://github.com/ShawnYoung72443'><i class="fab fa-github"></i>Shawn Young</a>
       </p>
       <p className='bg-dark p'>
         <strong>Version</strong> 1.0.0
